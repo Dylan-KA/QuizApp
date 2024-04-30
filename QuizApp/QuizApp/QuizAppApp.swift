e4c18ecf9023c31849bce07e59dedd4e14b840b6
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2024-04-30 10:39:22 +1000

    Added xCode Project

---
+++ QuizApp/QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Dylan Archer on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
