3eebb1761d8bd15d9318a808ddec959a37572dfa
Ludens <keegen.wallis@gmail.com>
2024-05-07 12:42:28 +1000

    Welcome page initial design
    Welcome page initial design with navigation button functionality

---
+++ QuizApp/QuizApp/LeaderboardView.swift
@@ -0,0 +1,19 @@
+//
+//  LeaderboardView.swift
+//  QuizApp
+//
+//  Created by Keegen Wallis on 7/5/2024.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+    var body: some View {
+        Spacer()
+        Text("Leaderboard")
+        Spacer()
+    }
+}
+#Preview {
+    LeaderboardView()
+}
