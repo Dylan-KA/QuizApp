f011cca38dc2d8cd3c619880c97fe235cc45500d
ShakAttack123321 <168495379+ShakAttack123321@users.noreply.github.com>
2024-05-12 18:51:59 +1000

    Controller Update

--- QuizApp/QuizApp/Utility/Utilities.swift
+++ QuizApp/QuizApp/Utility/Utilities.swift
@@ -14,21 +14,27 @@ final class Utilities {
     private init() {}
     
     @MainActor
-        func topViewController(controller: UIViewController? = nil) -> UIViewController? {
-            
-            let controller = controller ?? UIApplication.shared.keyWindow?.rootViewController
-            
-            if let navigationController = controller as? UINavigationController {
-                return topViewController(controller: navigationController.visibleViewController)
+    
+    func topViewController(controller: UIViewController? = nil) -> UIViewController? {
+        let controller = controller ?? UIApplication.shared.connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .compactMap { $0 as? UIWindowScene }
+            .first?.windows
+            .filter { $0.isKeyWindow }
+            .first?.rootViewController
+
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
             }
-            if let tabController = controller as? UITabBarController {
-                if let selected = tabController.selectedViewController {
-                    return topViewController(controller: selected)
-                }
-            }
-            if let presented = controller?.presentedViewController {
-                return topViewController(controller: presented)
-            }
-            return controller
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
     }
+
 }
