6789c5d03a30134e36f22457f7f4c4c031e19f25
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2024-05-13 17:57:27 +1000

    Fixed Welcome View
    Had multiple back buttons

--- QuizApp/QuizApp/WelcomeView.swift
+++ QuizApp/QuizApp/WelcomeView.swift
@@ -9,48 +9,48 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                Spacer()
+        VStack {
+            Spacer()
 
-                Text("Welcome")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
+            Text("Welcome")
+                .font(.largeTitle)
+                .fontWeight(.bold)
 
-                Divider()
-                    .frame(width: 250, height: 4)
-                    .background(Color.black)
-                    .padding(.vertical, 20)
+            Divider()
+                .frame(width: 250, height: 4)
+                .background(Color.black)
+                .padding(.vertical, 20)
 
-                Spacer()
+            Spacer()
 
-                NavigationLink(destination: QuizSettingsView()) {
-                    Text("Start a Quiz")
-                }
-                .padding()
-                .background(Color.green)
-                .foregroundColor(.white)
-                .cornerRadius(8)
-                .padding()
-
-                NavigationLink(destination: LeaderboardView()) {
-                    Text("Leaderboards")
-                }
-                .padding()
-                .background(Color.green)
-                .foregroundColor(.white)
-                .cornerRadius(8)
-                .padding()
-                
-                Spacer()
+            NavigationLink(destination: QuizSettingsView()) {
+                Text("Start a Quiz")
             }
-            .padding(70)
-            .background(Color(.systemGray6))
-            .edgesIgnoringSafeArea(.all)
+            .padding()
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .padding()
+
+            NavigationLink(destination: LeaderboardView()) {
+                Text("Leaderboards")
+            }
+            .padding()
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .padding()
+                
+            Spacer()
         }
+        .padding(70)
+        .background(Color(.systemGray6))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 #Preview {
-    WelcomeView()
+    NavigationView {
+        WelcomeView()
+    }
 }
