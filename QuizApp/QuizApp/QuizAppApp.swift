19cbacede0646719c1108fc42eb350b3800bc1ee
ShakAttack123321 <168495379+ShakAttack123321@users.noreply.github.com>
2024-05-13 21:06:41 +1000

    New Login
    Users can now create and account and log back into an account. Welcome view also displays username of player

--- QuizApp/QuizApp/QuizAppApp.swift
+++ QuizApp/QuizApp/QuizAppApp.swift
@@ -12,34 +12,11 @@ import FirebaseAppCheck
 @main
 struct QuizAppApp: App {
    
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
         WindowGroup {
             NavigationStack {
-                RootView()
+                EntryView()
             }
         }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        
-        // Setting up Firebase App Check only for real devices, not simulators
-        #if targetEnvironment(simulator)
-        print("Firebase App Check is not enabled on simulator.")
-        #else
-        // This sets up the DeviceCheck provider for App Check on real devices
-        if #available(iOS 14, *) {
-            AppCheck.setAppCheckProviderFactory(DeviceCheckProviderFactory())
-        } else {
-            // Handle older iOS versions if necessary
-            print("DeviceCheck is not available on iOS versions older than 14.")
-        }
-        #endif
-        
-        return true
     }
 }
